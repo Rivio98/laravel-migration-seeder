781c199b0057ab343e305a5231cd50910532e681
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-26 16:13:10 +0200

    [UPD]Compiled table with seed in database

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -4,16 +4,28 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Factory as Faker;
 
 class TrainSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        //
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();
+            $train->azienda = $faker->randomElement(['Trenitalia', 'Italo', 'Frecciarossa', 'Regionale']);
+            $train->stazione_partenza = $faker->city;
+            $train->stazione_arrivo = $faker->city;
+            $train->orario_partenza = $faker->time($format = 'H:i:s');
+            $train->orario_arrivo = $faker->time($format = 'H:i:s');
+            $train->codice_treno = $faker->regexify('[A-Z0-9]{6}');
+            $train->numero_carrozze = $faker->numberBetween(3, 12);
+            $train->in_orario = $faker->boolean;
+            $train->cancellato = $faker->boolean(20);
+            $train->save();
+        }
     }
 }
